14b29b9185139c635f25589f3ffdd1d7d7267ed3
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-11-30 20:18:22 +0100

    cases

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    res = ""
-    for lalpha in range(97, 123):
-        res = res + ord(lalpha)
-    if c in res:
-        #print("{} is lower".format(c))
-        return False
-    elif c not in res:
-        #print("{} is upper".format(c))
+    if ord(c) > 96:
         return True
+    else:
+        return False
