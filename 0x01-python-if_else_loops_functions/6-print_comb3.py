dac86f73ecac0955d2e11cff793477b7d2c152d2
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-11-30 19:31:34 +0100

    combo 2 digit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i < j:
             if i != 8 or (i == 8 and j != 9):
-                print(f"{i}{j},", end=" ")
+                print("{}{},".format(i, j), end=" ")
             if i == 8 and j == 9:
-                print(f"{i}{j}")
+                print("{}{}".format(i, j))
