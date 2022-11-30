698c94b6fe38e395c923ce987849c9689f6e4e73
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-11-30 14:12:23 +0100

    advance integer state checker

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    if number % 10 > 5:
+        hold = number % 10
+        print(f"Last digit of {number} is {hold} and is greater than 5")
+elif number % 10 == 0:
+    hold = number % 10
+    print(f"Lats digit of {number} is {hold} and is 0")
+elif number > 0 and number % 10 < 6:
+    hold = number % 10
+    print(f"Last digit of {number} is {hold} and is less than 6 and not zero")
+if number < 0 and number % -10 < 0:
+    hold = number % -10
+    print(f"Lats digit of {number} is {hold} and is less than 6 and not zero")
