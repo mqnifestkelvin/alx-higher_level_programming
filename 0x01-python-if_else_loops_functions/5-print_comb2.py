a8c3bacaf90b1899fc7333f3fa8b6f01425c7743
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-11-30 18:18:23 +0100

    number 1-99 with commas and space

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0{:d},".format(i), end=" ")
-    if i > 9:
-        print("{:d},".format(i), end=" ")
+    if i < 99:
+        print("{:02d},".format(i), end=" ")
+    if i == 99:
+        print("{:d}".format(i), end=" ")
