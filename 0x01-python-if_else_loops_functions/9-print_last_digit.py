76ff7437bbbaca533274463aaf3344e5d3899f24
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-11-30 21:27:53 +0100

    print last number

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number > 0:
+        number %= 10
+        return number
+    if number < 0:
+        number %= -10
+        return number * -1
+    if number == 0:
+        number %= 10
+        return number
