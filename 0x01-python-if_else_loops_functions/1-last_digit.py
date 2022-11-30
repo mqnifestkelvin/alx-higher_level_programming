bdb2370ead7892a81eb78008a5e0d1ce4caa4209
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-11-30 14:20:55 +0100

    advance integer state checker

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if number > 0:
         print(f"Last digit of {number} is {hold} and is greater than 5")
 elif number % 10 == 0:
     hold = number % 10
-    print(f"Lats digit of {number} is {hold} and is 0")
+    print(f"Last digit of {number} is {hold} and is 0")
 elif number > 0 and number % 10 < 6:
     hold = number % 10
     print(f"Last digit of {number} is {hold} and is less than 6 and not 0")
