99eb7e043c0050fd016255e214b16a1a8a3c8a79
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-12-01 14:18:54 +0100

    modules

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyhon3
 def add(a, b):
     return a + b
-if __name__ == "__main__":
+"""if __name__ == "__main__":
     import sys
-    add(int(sys.argv[1]), int(sys.argv[2]))
+    add(int(sys.argv[1]), int(sys.argv[2]))"""
