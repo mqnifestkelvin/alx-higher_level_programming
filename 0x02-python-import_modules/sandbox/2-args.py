e99d25c2e90cb50fe40571958376be2e2baadeb9
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-12-01 17:59:56 +0100

    a program that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/sandbox/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 1:
+        print(f"{count} argument")
+    elif count == 0:
+        print(f"{count} arguments.")
+    elif count > 1:
+        print(f"{count} arguments")
+    for number in range(1, count + 1):
+        print(f"{number}: {sys.argv[number]}")
+        
+
