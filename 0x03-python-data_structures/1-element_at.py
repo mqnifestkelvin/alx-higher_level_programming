a099aee9fecaec298893632405765d4874fc6dcc
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-12-04 18:34:49 +0100

    debugging

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     element = my_list[idx]
-    index = idx
-    print("element at index {} is {}".format(index, element))
-element_at([1,2,3,4,5], 4)
+    print("element at index {} is {}".format(idx, element))
