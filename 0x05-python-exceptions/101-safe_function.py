87677522041c284e3cdcfd09ccd73c3892546ea2
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-12-19 22:00:10 +0100

    A function that executes a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,5 +7,5 @@ def safe_function(fct, *arg):
         hold = fct(*arg)
         return hold
     except:
-        print("Exception: {}".format(sys.exc_info()[1], file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
