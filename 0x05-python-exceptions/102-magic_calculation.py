33f9e307f5ec7e9d03ed71f113c464f610741fc8
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-12-19 22:08:25 +0100

    def magic_calculation(a, b):

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    hold = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                hold += a ** b / i
+        except:
+            hold = b + a
+            break
+    return (result)
