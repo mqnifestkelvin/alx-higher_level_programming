1198225e0305d658e4bc0934992b13b90258d610
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-12-20 16:19:17 +0100

    a class Square that defines a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """Construct of a square"""
 
     def __init__(self, size=0)
-    """Constructor.
+        """Constructor.
 
         args:
             Size: The length of the sides of the square.
@@ -14,7 +14,7 @@ class Square:
         Raises:
             TypeError: The arguement size must be an integer.
             ValueError: The arguement passed must be greater than Zero (0)
-    """
+        """
         if not isinstance(size, int):
         raise TypeError("size must be an integer")
         if size < 0:
