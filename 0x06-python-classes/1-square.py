93002c9d50e86a7f98d5f32cc63a90434b5984b4
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-12-20 15:51:54 +0100

    a class Square that defines a square by:\n 1. Private instance attribute: size\n 2. Instantiation with size (no type/value verification

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A square module."""
+
+class Square:
+    def __init__(self,  size=0):
+        """Constructor.
+
+        args:
+            size: The length of all sizes of a square.
+
+        Raises:
+            TypeError: If the size arguement is not an integer
+            ValueError: If the size arguement is less than Zero (0)
+        """
+        if not instance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
