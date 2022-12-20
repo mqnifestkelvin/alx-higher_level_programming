fec8e64df942e2f9a9a68359e750b15e15dbdce1
mqnifestkelvin <mqnifestkelvin@gmail.com>
2022-12-20 19:38:44 +0100

    a class Square that defines a square, added area method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+class Square:
+    def __init__(self,size=0):
+        """Constructor.
+        Args:
+            size: The size of the square.
+            
+        Raise:
+            TypeError: The argument passed is not and integer.
+            ValueError: The arguement passed is less than Zero (0).
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__self = size
+
+    def area(self):
+        """Area of the square passed.
+
+        Returns:
+            The area of the square
+        """
+        return self.__self ** 2
