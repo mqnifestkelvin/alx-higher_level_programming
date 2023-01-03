d132ce96480ab6855771fb30cdcab7fc14160700
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-01-03 17:34:55 +0100

    made it printable with __str__

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Module for Rectangle class."""
+
+
+class Rectangle:
+    """This class defines a simple Rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Constructor.
+        Args:
+            width: The width of the rectangle.
+            height: The height of a rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Property for the width of the rectangle.
+        Raises:
+            TypeError: if the width is not an integer.
+            ValueError: if the width is less than zeror (0).
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Property of the height of a rectangle.
+        Raises:
+            TypeError: if the height is not an integer.
+            ValueError: if the height is less than zero (0).
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the area of an instantiated Rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Returns the perimeter of an instantiated Rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Returns a string representation of the Rectangle"""
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            return (("#" * self.width + "\n") * self.height)[:-1]
