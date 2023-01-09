52dd0ccf65a29aecbb709ed892775ec253324e36
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-01-09 17:37:47 +0100

    a class MyList that inherits from list

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
+'''Module for is_kind_of_class method.'''
+
+
 def is_kind_of_class(obj, a_class):
-    """Checks if an object is a subclass of an instance.
-    Args:
-        obj: The object to be copared.
-        a_class: The class to compare the object to.
-    Return:
-        If an object is an instance or inherited instance of a class -True
-        Otherwise - False
-    """
+    '''Determines if an object is a subclass of a class.'''
     return isinstance(obj, a_class)
