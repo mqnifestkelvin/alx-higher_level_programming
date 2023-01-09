5996e74884b8c231ec665d66a5b9a25efc413ac3
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-01-09 14:59:27 +0100

    a function that returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """Module for method lookup."""
 
+
 def lookup(arg):
-    """This function returns a list of all the available methods and attributes.
+
+    """This function returns a list of all the
+    available methods and attributes.
 
         Args:
             arg (object): inbuild or user class object.
 
         Returns:
-            list: Returnss a list of all the attributes and methods available in a class. 
+            list: Returnss a list of all the attributes
+            and methods available in a class.
     """
     return dir(arg)
