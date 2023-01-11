411124319e7c9e7e28a4eebb3eab9d329dc0b8ec
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-01-11 18:32:54 +0100

    a class student that defines a stydent

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,10 +6,10 @@ import json
 class Student:
     """A construct of an object Student."""
 
-    def __init__(self, first_name, last_name, age)
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """Get a dictionary representation of the Student."""
