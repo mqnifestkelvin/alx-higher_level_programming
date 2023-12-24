dcc2e33a13afa8fe08af28b2818c490efe0bd811
mqnifestkelvin <mqnifestkelvin@gmail.com>
2023-12-24 18:50:38 +0100

    corrected code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,15 +2,31 @@
 """Lists all states"""
 
 import MySQLdb
-from sys import argv
+import sys
+
+def list_states(username, password, db_name):
+    # Connect to the MySQL database
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
+
+    # Execute the SQL query to select all states
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows from the query
+    states = cur.fetchall()
+
+    # Print the states
+    for state in states:
+        print(state)
+
+    # Close the cursor and the database connection
+    cur.close()
+    db.close()
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                           user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    if len(sys.argv) == 4:
+        list_states(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
