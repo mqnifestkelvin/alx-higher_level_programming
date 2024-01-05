5490235ba71e4ea76a7aaa02a44adf7d065197fb
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-01-05 23:35:27 +0100

    submit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Finds the peak in an unsorted list of integers"""
+"""Finds peak in unsorted list of integers"""
 
 
 def find_peak(list_of_integers):
-    """Finds the peak in an unsorted list of ints"""
+    """Finds peak in unsorted list of integers"""
+
     if len(list_of_integers) > 1:
         if list_of_integers[0] >= list_of_integers[1]:
             return list_of_integers[0]
