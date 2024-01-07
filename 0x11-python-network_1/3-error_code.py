504270fa639c0efdb429eb695f25b62f09c0c8ad
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-01-07 22:26:40 +0100

    submit

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-""" Fetches header from url passed as arg & handles errors a bit"""
-from urllib import request, parse, error
+"""
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+(handling HTTP errors)
+"""
+from urllib import request, error
 import sys
+
 
 if __name__ == "__main__":
     try:
         with request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode('utf-8'))
-    except error.HTTPError as e:
-        print("Error code:", e.code)
+            body = response.read()
+            print(body.decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
