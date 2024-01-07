38d6b44cf06fa6361cff26f882924765ca048880
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-01-07 22:28:08 +0100

    submit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""" Uses requests module -- yay"""
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
+
 if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
