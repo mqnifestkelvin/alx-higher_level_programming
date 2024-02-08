50fc59d477ee0d41c810e6acfcd73413922a855a
mqnifestkelvin <mqnifestkelvin@gmail.com>
2024-02-08 04:55:55 +0100

    submit

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () =>
+  $("INPUT#btn_translate").click(() =>
+    $.get(
+      "https://www.fourtonfish.com/hellosalut/hello/?lang=" +
+        $("INPUT#language_code").val(),
+      (data) => $("DIV#hello").html(data.hello)
+    )
+  )
+);
